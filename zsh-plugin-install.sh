f427cb7962cacd550e7281ecd91e4b3e82ffcaa3
Dan Cochrane <danc@speechmatics.com>
2022-01-10 09:54:29 +0000

    add plugin install script

---
+++ zsh-plugin-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && echo "Cloned autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting && echo "Cloned syntax highlighting"
+
