34e8784b4239f251cc63e405a6469246773b0791
Dan Cochrane <danc@speechmatics.com>
2022-01-10 10:19:05 +0000

    add the git clone commands

--- zsh-plugin-install.sh
+++ zsh-plugin-install.sh
@@ -4,3 +4,4 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && echo "Cloned autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting && echo "Cloned syntax highlighting"
 
+# source the plugins
