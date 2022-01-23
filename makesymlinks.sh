c2160ccc1a300fb665eb5cbf4f54de9736634665
Dan Cochrane <dpc46@Dans-PC.localdomain>
2022-01-23 18:50:24 +0000

    Change files for symlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,16 +6,15 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=~/.config                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc p10k gitconfig vimrc"    # list of files/folders to symlink in homedir
+files="zshrc p10k.zsh "    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+mkdir -p $olddir && echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
@@ -29,33 +28,3 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
-
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
-
-install_zsh
