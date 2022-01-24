2d5f86d2096c4e2d18260ce2c25f5bd1dabbe303
Dan Cochrane <danc@speechmatics.com>
2022-01-24 11:41:32 +0000

    Added p10k to install

--- zsh-plugin-install.sh
+++ zsh-plugin-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k && "Installed p10k"
+
 # install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && echo "Cloned autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting && echo "Cloned syntax highlighting"
