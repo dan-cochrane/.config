8edf9f6f34bc7df5ca4a9a05aed45093a1ecadfd
Dan Cochrane <danc@speechmatics.com>
2022-02-01 10:20:22 +0000

    Add latest nvim release

--- zsh-plugin-install.sh
+++ zsh-plugin-install.sh
@@ -7,4 +7,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && echo "Cloned autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting && echo "Cloned syntax highlighting"
 
-# source the plugins
+# clone the .config repo
+[ -d ~/.config ] && mv ~/.config ~/.config_old
+git clone git@github.com:dpc46/.config.git
+
+# install the latest stable nvim release and add to path
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+sudo ln -s ~/nvim.appimage /usr/bin/nvim
