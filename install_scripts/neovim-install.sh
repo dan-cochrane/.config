e87c50628212ab8857a17c562d3b83737bc08e5f
Dan Cochrane <danc@speechmatics.com>
2022-04-25 11:25:58 +0100

    Update to nvim 0.7.0

--- install_scripts/neovim-install.sh
+++ install_scripts/neovim-install.sh
@@ -2,10 +2,10 @@
 
 cd ~
 sudo apt-get remove --auto-remove neovim && echo "Removed Neovim..."
-curl -LO https://github.com/neovim/neovim/releases/download/v0.6.1/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage nvim
 sudo mv nvim /usr/local/bin
 
-echo "Installed neovim v0.6.1..."
+echo "Installed neovim v0.7.0..."
 
