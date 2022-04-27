0e1181cd9120f31914976072dfde6e449f9af262
Dan Cochrane <danc@speechmatics.com>
2022-04-27 16:31:20 +0100

    Tab width 4, fix nvim-tree

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -9,10 +9,20 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
+  on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+        end
+    end,
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.prettier.with({ extra_args = { "--no-semi", "--print-width 120" } }),
+		formatting.black.with({ extra_args = { "--line-length 120" } }),
 		formatting.stylua,
     -- diagnostics.flake8
 	},
