1ce4aea32982db3902914ce592954b2ff750a7d9
Dan <danpcochrane@gmail.com>
2022-07-09 16:00:09 +0100

    Initial Commit

---
+++ config/key_binds.sh
@@ -0,0 +1,69 @@
+# git add ci and push
+function git_prepare() {
+   if [ -n "$BUFFER" ]; then
+	BUFFER="git add -u && git commit -m \"$BUFFER\" "
+   fi
+
+   if [ -z "$BUFFER" ]; then
+	BUFFER="git add -u && git commit -v "
+   fi
+		
+   zle accept-line
+}
+zle -N git_prepare
+bindkey -r "\eg"
+bindkey "\eg" git_prepare
+
+function prepend-sudo {
+  if [[ $BUFFER != "sudo "* ]]; then
+    BUFFER="sudo $BUFFER"; CURSOR+=5
+    zle reset-prompt
+  fi
+}
+zle -N prepend-sudo
+bindkey "\es" prepend-sudo
+
+# Copied from .fzf.zsh and modifed to only return files
+__fsel_files() {
+  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type f -print \
+    -o -type l -print 2> /dev/null | cut -b3-"}"
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local item
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read item; do
+    echo -n "${(q)item} "
+  done
+  local ret=$?
+  echo
+  return $ret
+}
+
+function fzf-vim {
+    selected=$(__fsel_files)
+    if [[ -z "$selected" ]]; then
+        zle redisplay
+        return 0
+    fi
+    zle push-line # Clear buffer
+    BUFFER="v $selected";
+    zle accept-line
+}
+zle -N fzf-vim
+bindkey "^v" fzf-vim
+
+# Set Up and Down arrow keys to the (zsh-)history-substring-search plugin
+# `-n` means `not empty`, equivalent to `! -z`
+[[ -n "${terminfo[kcuu1]}" ]] && bindkey "${terminfo[kcuu1]}" history-substring-search-up
+[[ -n "${terminfo[kcud1]}" ]] && bindkey "${terminfo[kcud1]}" history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
+
+# Accept zsh autosggestions with ctrl + space
+# Make sure you don't have mac input source switch
+bindkey '^ ' autosuggest-accept  
+
+# To match my custom vim bindings
+bindkey -M vicmd 'H' beginning-of-line
+bindkey -M vicmd 'L' end-of-line
