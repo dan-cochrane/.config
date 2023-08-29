9f9207fd77d9603a1c3e81da129f7a02d74a87b3
Daniel Cochrane <danc@speechmatics.com>
2023-08-29 13:38:21 +0100

    Add singularity prompt for p10k

--- config/p10k.zsh
+++ config/p10k.zsh
@@ -48,6 +48,7 @@
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    singularity             # shows when inside a singlularity environment
     anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
@@ -1669,6 +1670,14 @@
   # Type `p10k help segment` for documentation and a more sophisticated example.
   function prompt_example() {
     p10k segment -b 1 -f 3 -i '⭐' -t 'hello, %n'
+  }
+
+  function prompt_singularity() {
+    if [ ! -z "$SINGULARITY_CONTAINER" ]; then
+      name=$(echo ${SINGULARITY_CONTAINER} | awk -F/ '{print $(NF-0)}')
+      [[ ! -z "$SIF_OVERLAY" ]] && icon='💫 🚧' || icon='💫'
+      p10k segment -f 031 -i "${icon}" -t "${name}"
+    fi
   }
 
   # User-defined prompt segments may optionally provide an instant_prompt_* function. Its job
